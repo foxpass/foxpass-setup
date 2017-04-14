ea11fe7f0f3f0d04d30a491a4c389c64a5cfe3b9
Travis Theune <travist@tenminutesout.net>
2017-04-14 18:44:01 +0200

    Changing uri.startswith to uris[0].startswith

--- linux/debian/8/foxpass_setup.py
+++ linux/debian/8/foxpass_setup.py
@@ -160,7 +160,7 @@ tls_cacertfile /etc/ssl/certs/ca-certificates.crt
 nss_initgroups_ignoreusers ALLLOCAL
 """
         sslstatus='off'
-        if uri.startswith('ldaps://'):
+        if uirs[0].startswith('ldaps://'):
             sslstatus='on'
         w.write(content.format(uris='\nuri '.join(uris), basedn=basedn, binddn=binddn,
                                bindpw=bindpw, sslstatus=sslstatus, threads=threads))
