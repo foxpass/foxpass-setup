c029111cfa4ff808381e19e15b096ca6087f4c47
Christian Becker <c.becker@mes-info.de>
2017-04-30 15:17:33 +0200

    Ignore _mbsetupuser

--- macosx/elcapitan/FoxpassLoginHook.bash
+++ macosx/elcapitan/FoxpassLoginHook.bash
@@ -2,7 +2,7 @@
 LOGFILE=/var/log/loginhook.log
 echo "$(date) Login hook exected for user $1" >> $LOGFILE
 
-if [ ! -z "$1" ] && [ ! -d /Users/$1 ]; then
+if [ ! -z "$1" ] && [ "_mbsetupuser" != "$1" ] && [ ! -d /Users/$1 ]; then
   echo "$(date) Adding user $1" >> $LOGFILE
   mkdir -p /Users/$1
   /usr/sbin/chown $1:staff /Users/$1
