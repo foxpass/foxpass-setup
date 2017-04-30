7ed006b5b1704929d8a8ab5d56ced9d4404f849c
Christian Becker <c.becker@mes-info.de>
2017-04-30 15:16:52 +0200

    Added logging

--- macosx/elcapitan/FoxpassLoginHook.bash
+++ macosx/elcapitan/FoxpassLoginHook.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
+LOGFILE=/var/log/loginhook.log
+echo "$(date) Login hook exected for user $1" >> $LOGFILE
 
 if [ ! -z "$1" ] && [ ! -d /Users/$1 ]; then
+  echo "$(date) Adding user $1" >> $LOGFILE
   mkdir -p /Users/$1
   chown $1:staff /Users/$1
 fi
