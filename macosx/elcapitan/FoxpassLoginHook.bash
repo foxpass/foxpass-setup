f25feb260f271dcd803f197fbfdc4459fae0519f
Christian Becker <c.becker@mes-info.de>
2017-04-30 15:17:15 +0200

    Fixed chown path

--- macosx/elcapitan/FoxpassLoginHook.bash
+++ macosx/elcapitan/FoxpassLoginHook.bash
@@ -5,5 +5,5 @@ echo "$(date) Login hook exected for user $1" >> $LOGFILE
 if [ ! -z "$1" ] && [ ! -d /Users/$1 ]; then
   echo "$(date) Adding user $1" >> $LOGFILE
   mkdir -p /Users/$1
-  chown $1:staff /Users/$1
+  /usr/sbin/chown $1:staff /Users/$1
 fi
