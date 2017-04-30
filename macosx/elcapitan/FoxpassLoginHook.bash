b85c88a89027b4ce1966beada9a9c919e1734b51
Christian Becker <c.becker@mes-info.de>
2017-04-30 15:17:54 +0200

    Configure mobile account

--- macosx/elcapitan/FoxpassLoginHook.bash
+++ macosx/elcapitan/FoxpassLoginHook.bash
@@ -6,4 +6,5 @@ if [ ! -z "$1" ] && [ "_mbsetupuser" != "$1" ] && [ ! -d /Users/$1 ]; then
   echo "$(date) Adding user $1" >> $LOGFILE
   mkdir -p /Users/$1
   /usr/sbin/chown $1:staff /Users/$1
+  /System/Library/CoreServices/ManagedClient.app/Contents/Resources/createmobileaccount -n $1 -v >> $LOGFILE
 fi
