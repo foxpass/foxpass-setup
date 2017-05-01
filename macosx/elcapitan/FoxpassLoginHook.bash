6e456529a517c6f35462d7ee864286977ff87d49
Christian Becker <chris@beckr.org>
2017-05-01 21:39:28 +0200

    macOS login hook improvements (#17)
    * Added logging
    
    * Fixed chown path
    
    * Ignore _mbsetupuser
    
    * Configure mobile account
    
    * Fixed typo in FoxpassLoginHook.bash

--- macosx/elcapitan/FoxpassLoginHook.bash
+++ macosx/elcapitan/FoxpassLoginHook.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
+LOGFILE=/var/log/loginhook.log
+echo "$(date) Login hook executed for user $1" >> $LOGFILE
 
-if [ ! -z "$1" ] && [ ! -d /Users/$1 ]; then
+if [ ! -z "$1" ] && [ "_mbsetupuser" != "$1" ] && [ ! -d /Users/$1 ]; then
+  echo "$(date) Adding user $1" >> $LOGFILE
   mkdir -p /Users/$1
-  chown $1:staff /Users/$1
+  /usr/sbin/chown $1:staff /Users/$1
+  /System/Library/CoreServices/ManagedClient.app/Contents/Resources/createmobileaccount -n $1 -v >> $LOGFILE
 fi
