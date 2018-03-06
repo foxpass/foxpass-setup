0ba1866966cea56988d31122d102915412a036e2
Travis Theune <travist@delmarvelo.us>
2018-03-06 06:43:04 +0100

    Change foxpass_ssh_key exit status if user exists locally (#31)

--- linux/ubuntu/17.04/foxpass_setup.py
+++ linux/ubuntu/17.04/foxpass_setup.py
@@ -105,7 +105,7 @@ def write_foxpass_ssh_keys_script(apis, api_key):
 user="$1"
 secret="%s"
 hostname=`hostname`
-if grep -q "^${user}:" /etc/passwd; then exit 1; fi
+if grep -q "^${user}:" /etc/passwd; then exit; fi
 aws_instance_id=`curl -s -q -f http://169.254.169.254/latest/meta-data/instance-id`
 aws_region_id=`curl -s -q -f http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/.$//'`
 %s
@@ -120,7 +120,7 @@ exit $?
 user="$1"
 secret="%s"
 hostname=`hostname`
-if grep -q "^${user}:" /etc/passwd; then exit 1; fi
+if grep -q "^${user}:" /etc/passwd; then exit; fi
 %s
 exit $?
 """
